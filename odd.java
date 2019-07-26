418a63814294931e0a9acf81af4aa14cf66750ce
ranashis <anshfactor@gmail.com>
2019-07-26 11:50:33 +0530

    lets code

---
+++ odd.java
@@ -0,0 +1,15 @@
+class OddNum
+{
+	public static void main (String args[])
+	{
+		for (int i=0;i<=100;i++)
+		{
+			if (i%2!=0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+		System.out.println("All the odd numbers");
+	}
+}
+
